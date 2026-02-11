0d5dadf13d1da4a7936c71bfe37e02d6e79bfa25
ajinkya-petabyte <mirgudew@gmail.com>
2026-02-11 13:18:40 +0530

    Update main.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -16,8 +16,28 @@ provider "aws" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_name          = "enterprise-dev-vpc"
-  cidr              = "10.0.0.0/16"
+  vpc_name           = "enterprise-dev-vpc"
+  cidr               = "10.0.0.0/16"
   public_subnet_cidr = "10.0.1.0/24"
-  az                = "ap-south-1a"
+  az                 = "ap-south-1a"
 }
+
+module "security_group" {
+  source = "../../modules/security-group"
+
+  name             = "enterprise-dev-sg"
+  vpc_id           = module.vpc.vpc_id
+  allowed_ssh_cidr = ["0.0.0.0/0"]
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  name              = "enterprise-dev-server"
+  ami               = "ami-0f58b397bc5c1f2e8"
+  instance_type     = "t2.micro"
+  subnet_id         = module.vpc.public_subnet_id
+  security_group_id = module.security_group.security_group_id
+  key_name          = "your-key-name"
+}
+
